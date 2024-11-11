4d837afe2f06d8bcb1690d5b8373662678c3a033
zacklee1909 <huynhao1909@gmail.com>
2024-11-11 19:36:40 +0700

    test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 st.title("Python project")
-with st.form("Điền vào chỗ cần điền"):
+with st.form(key='user_form'):
     name = st.text_input("Họ và tên")
     age = st.text_input("Tuổi")
     work = st.text_input("Nghề nghiệp")
@@ -10,6 +10,6 @@ with st.form("Điền vào chỗ cần điền"):
     gift = st.text_input("Gửi lời cho người làm code")
     send = st.form_submit_button("Send")
 if send:
-    print(f"{name},{age},{work},{favor},{sdt},{gift}")
-    st.success(f"Cảm ơn {name} rất nhiều.")
+    st.write(f"{name},{age},{work},{favor},{sdt},{gift}")
+    st.success(f"Cảm ơn rất nhiều.")
     
