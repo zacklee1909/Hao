c05301a903ae6ebf6dbc9dc20422f2d434e5bae6
zacklee1909 <huynhao1909@gmail.com>
2024-11-11 17:40:48 +0700

    test.py

--- test.py
+++ test.py
@@ -9,7 +9,6 @@ with st.form("Điền vào chỗ cần điền"):
     gift = st.text_input("Gửi lời cho người làm code")
     send = st.form_submit_button("Gửi")
 if send:
-    st.success(f"Cảm ơn {name} rất nhiều.")
     data = {
         "Name": [name],
         "Age": [age],
@@ -20,9 +19,4 @@ if send:
     df = pd.DataFrame(data)
     print("{name},{age},{work},{favor},{gift}")
     df.to_csv("form_data.csv",mode='a',header=False,index=False)
-    st.write("Information has been saved in file form_data.csv")
-try:
-    form_data = pd.read_csv("form_data.csv",names=["Name","Age","Work","Favorite","Gift"],header=None)
-    st.write(form_data)
-except FileNotFoundError:
-    pass
+    st.success(f"Cảm ơn {name} rất nhiều.")
